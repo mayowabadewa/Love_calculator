c595e80d666c511b82ea127cc8d751ec139c42e5
mayowabadewa <100229789+mayowabadewa@users.noreply.github.com>
2022-09-21 20:53:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+
+names = (name1 + " " + name2)
+
+names = names.lower()
+t = names.count("t")
+r = names.count("r")
+u = names.count("u")
+e = names.count("e")
+
+l = names.count("l")
+o = names.count("o")
+v = names.count("v")
+
+true_count = str(t + r + u + e)
+
+love_count = str(l + o + v+ e)
+
+love_score = true_count + love_count
+
+love_score_as_int = int(love_score)
+
+if love_score_as_int < 10 or love_score_as_int > 90:
+  print(f"Your score is {love_score_as_int}, you go together like coke and mentos.")
+elif love_score_as_int > 40 and love_score_as_int < 50:
+  print(f"Your score is {love_score_as_int}, you are alright together.")
+else:
+  print(f"Your score is {love_score_as_int}.")
